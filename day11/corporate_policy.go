8b45a458f1e7fc9784a78265123cd521a255c558
Nick Klauer <klauer@gmail.com>
2016-01-02 15:41:48 -0600

    complete day 11

--- day11/corporate_policy.go
+++ day11/corporate_policy.go
@@ -1,16 +1,33 @@
 package main
 
-import "strings"
+import (
+	"strings"
+	"fmt"
+)
 
 func main() {
-
+	fmt.Println("Next Password for \"hepxcrrq\"", NextPassword("hepxcrrq"))
 }
 
 func NextPassword(current string) string {
+	password := current
+	for  {
+		password = Increment(password)
+		if HasIncreasingLetters(password) && !ContainsInvalidChars(password) && ContainsTwoLetterPairs(password) {
+			return password
+		}
+	}
 	return ""
 }
 
 func HasIncreasingLetters(password string) bool {
+	out := []rune(password)
+	for i := 0; i <= len(out)-3; i++ {
+		// && out[i+2]+1 == out[i+3]
+		if out[i]+1 == out[i+1] && out[i+1]+1 == out[i+2] {
+			return true
+		}
+	}
 	return false
 }
 
@@ -24,25 +41,28 @@ func ContainsInvalidChars(password string) bool {
 	return false
 }
 
-func ContainsLetterPair(password string) bool {
+func ContainsTwoLetterPairs(password string) bool {
 	previous := ""
+	pairs := 0
+	matched := ""
 	for _, r := range password {
 		current := string(r)
-		if current == previous {
-			return true
+		if current == previous && matched != current {
+			pairs += 1
+			matched = current
 		}
 		previous = current
 	}
-	return false
+	return pairs >= 2
 }
 
 func Increment(password string) string {
 	out := []rune(password)
-	for i := len(out)-1; i >= 0; i-- {
+	for i := len(out) - 1; i >= 0; i-- {
 		if out[i] == 'z' {
 			out[i] = 'a'
 		} else {
-			out[i] = out[i]+1
+			out[i] = out[i] + 1
 			break
 		}
 	}
--- day11/corporate_policy_test.go
+++ day11/corporate_policy_test.go
@@ -2,10 +2,6 @@ package main
 
 import "testing"
 
-func TestNextPassword(t *testing.T) {
-
-}
-
 var incrementTests = []struct {
 	in  string
 	out string
@@ -14,14 +10,6 @@ var incrementTests = []struct {
 	{"az", "ba"},
 	{"abc", "abd"},
 	{"abz", "aca"},
-}
-
-var nextPasswordTests = []struct {
-	in  string
-	out string
-}{
-	{"abcdefgh", "abcdffaa"},
-	{"ghijklmn", "ghjaabcc"},
 }
 
 func TestIncrement(t *testing.T) {
@@ -37,14 +25,15 @@ var letterPairs = []struct {
 	in  string
 	out bool
 }{
-	{"aa", true},
+	{"aabb", true},
+	{"aaab", false},
 	{"abccdeff", true},
 	{"abcdefg", false},
 }
 
 func TestContainsLetterPair(t *testing.T) {
 	for _, tt := range letterPairs {
-		pass := ContainsLetterPair(tt.in)
+		pass := ContainsTwoLetterPairs(tt.in)
 		if pass != tt.out {
 			t.Errorf("ContainsLetterPair(\"%s\"): \tWanted %6s, Got %-6s", tt.in, tt.out, pass)
 		}
@@ -52,9 +41,9 @@ func TestContainsLetterPair(t *testing.T) {
 }
 
 var invalidStrings = []struct {
-in string
-out bool
-} {
+	in  string
+	out bool
+}{
 	{"abcdefghi", true},
 	{"hijklmmn", true},
 	{"abbceffg", false},
@@ -65,6 +54,43 @@ func TestInvalidStrings(t *testing.T) {
 		pass := ContainsInvalidChars(tt.in)
 		if pass != tt.out {
 			t.Errorf("ContainsInvalidChars(\"%s\"): \tWanted %6s, Got %-6s", tt.in, tt.out, pass)
+		}
+	}
+}
+
+var increasingLetters = []struct {
+	in  string
+	out bool
+}{
+	{"abc", true},
+	{"febecde", true},
+	{"hijklmmn", true},
+	{"abbcegjk", false},
+	{"abbceffg", false},
+}
+
+func TestIncreasingLetters(t *testing.T) {
+	for _, tt := range increasingLetters {
+		pass := HasIncreasingLetters(tt.in)
+		if pass != tt.out {
+			t.Errorf("HasIncreasingLetters(\"%s\"): \tWanted %v, Got %v", tt.in, tt.out, pass)
+		}
+	}
+}
+
+var nextPasswords = []struct {
+	in  string
+	out string
+}{
+	{"abcdefgh", "abcdffaa"},
+	{"ghijklmn", "ghjaabcc"},
+}
+
+func TestNextPassword(t *testing.T) {
+	for _, tt := range nextPasswords {
+		pass := NextPassword(tt.in)
+		if pass != tt.out {
+			t.Errorf("NextPassword(\"%s\"): \tWanted %s, Got %s", tt.in, tt.out, pass)
 		}
 	}
 }
