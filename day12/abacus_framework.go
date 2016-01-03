a06467c84cff9f3514d5bc2fee26caf914295660
Nick Klauer <klauer@gmail.com>
2016-01-03 10:46:11 -0600

    add type assertions and pull out array and map summing

--- day12/abacus_framework.go
+++ day12/abacus_framework.go
@@ -1,25 +1,83 @@
 package main
 
-import "fmt"
-
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
 
 func main() {
 
-	fmt.Println("Output for input is ")
+	file, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	var f interface{}
+	json.Unmarshal(file, &f)
+
+	PrintOutput(f)
+	fmt.Println("Output for input is ", Sum(f))
 }
 
-func Sum(input map[string]interface{}) int {
-	total := 0
+
+func PrintOutput(input interface{}) {
 	m := input.(map[string]interface{})
 	for k, v := range m {
+		switch vv := v.(type) {
+		case string:
+			fmt.Println(k, "is string", vv)
+		case int:
+			fmt.Println(k, "is int", vv)
+		case []interface{}:
+			fmt.Println(k, "is an array:")
+			for i, u := range vv {
+				fmt.Println(i, u)
+			}
+		case map[string]interface{}:
+			fmt.Println(k, "is a map:")
+		default:
+			fmt.Println(k, "is of a type I don't know how to handle")
+		}
+	}
+}
+
+func SumList(input []interface{}) int {
+	total := 0
+	for _, v := range input {
+		switch v.(type) {
+		case int:
+			total += v.(int)
+		}
+	}
+	return total
+}
+
+func SumMap(input map[string]interface{}) int {
+	total := 0
+	for k, v := range input {
 		switch vv := v.(type) {
 		case int:
 			total += vv
 		case []interface{}:
-			total += Sum(vv)
+			for _, vvv := range vv {
+				total += SumList(vvv.([]interface{}))
+			}
+		case map[string]interface{}:
+			total += SumMap(v.(map[string]interface{}))
 		default:
-			panic(fmt.Sprintf("Can't evaluate %v for %v", k, vv))
+			panic(fmt.Sprintf("Can't evaluate %v for %v as %v", k, v, vv))
 		}
+	}
+	return total
+}
+
+func Sum(input interface{}) int {
+	total := 0
+	switch input.(type) {
+	case map[string]interface{}:
+		total += SumMap(input.(map[string]interface{}))
+	case []interface{}:
+		total += SumList(input.([]interface{}))
 	}
 	return total
 }
